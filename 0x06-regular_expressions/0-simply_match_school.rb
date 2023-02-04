5cb020c9a1b424bfbc89f788e4f7032e516be17e
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2023-02-03 17:55:44 -0800

    match for school

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Accept command line argument
+string = ARGV[0]
+
+# Define the regular expression
+regex = /School/
+
+# Use the match method to check if the regular expression matches the string
+if string.match(regex)
+  puts "Match found"
+else
+  puts "No match found"
+end
